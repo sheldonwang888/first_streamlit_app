5b143f525b8c5d3a611d628f1eef7122a116bc76
sheldonwang888 <126528924+sheldonwang888@users.noreply.github.com>
2023-02-28 09:28:11 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,8 @@ import streamlit
 
 
 streamlit.header('New Diner')
-streamlit.title('Breakfirst Menu')
-streamlit.text('Omega 3')
-streamlit.text('Kale')
-streamlit.text('boiled egg')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
 
