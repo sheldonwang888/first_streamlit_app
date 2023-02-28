29d55de0a1ca8d5777eeafc2b8b39b3c2dccc56a
sheldonwang888 <126528924+sheldonwang888@users.noreply.github.com>
2023-02-28 10:19:05 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,8 +15,9 @@ my_first_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 my_first_list = my_first_list.set_index('Fruit')  # Fruit is the first column title in the fruit_macros.txt
 
 #Let's put a [ick list here so they can pick the fruit they want to include
-streamlit.multiselect("Pick some fruits:", list(my_first_list.index), ['Avocado', 'Strawberries'])
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_first_list.index), ['Avocado', 'Strawberries'])
+fruits_to_show = my_first_list.loc[fruits_selected]
 
 #display the table on the page
-streamlit.dataframe(my_first_list)
+streamlit.dataframe(fruits_to_show)
 
