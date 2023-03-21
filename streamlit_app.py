9a063e957304bb7b7f54f13c55ec800117cb007f
sheldonwang888 <126528924+sheldonwang888@users.noreply.github.com>
2023-03-20 21:33:50 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,4 +20,7 @@ import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
 
+streamlit.header("Fruityvice Fruit Advice!")
+
+
 
